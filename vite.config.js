f3cfaf92044d1508758a2b2992083afe06e761c4
Bhumib01 <bhumibapadhiyar@gmail.com>
2024-04-26 17:21:18 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss('./tailwind.config.js')],
+  base: "/portfolio/"
 })
